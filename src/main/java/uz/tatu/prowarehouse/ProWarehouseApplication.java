07876c595373fd55e01a8d44704eea842086c77e
yahyoHakimov <hakimovyahyo7@gmail.com>
2023-01-25 13:44:59 +0500

    all addedd

---
+++ src/main/java/uz/tatu/prowarehouse/ProWarehouseApplication.java
@@ -0,0 +1,13 @@
+package uz.tatu.prowarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/tatu/prowarehouse/ProWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tatu.prowarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
